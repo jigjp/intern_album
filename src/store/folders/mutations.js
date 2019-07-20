84c0d7020226b19cb1ad101020b0f9c6a66f60d3
yamada <yamada@jig.jp>
2019-07-20 16:50:29 +0900

    👍 state module rename dates -> folders

--- src/store/folders/mutations.js
+++ src/store/folders/mutations.js
@@ -6,3 +6,7 @@ export function someMutation (state) {
 export function setCurrent (state, payload) {
   state.current = payload
 }
+
+export function setFolders (state, payload) {
+  state.all = payload
+}
