1331ddc76700b72fc7981457d51b5e102c4eb7e2
yamada <yamada@jig.jp>
2019-08-21 22:20:03 +0900

    👍 画像URLのホストを環境変数から取得するように

--- src/store/pictures/actions.js
+++ src/store/pictures/actions.js
@@ -6,6 +6,10 @@ import { apiGetPictures } from '../../api/service'
 
 export function getPictures ({ commit }, folder) {
   return apiGetPictures(folder).then(res => {
-    commit('setPictures', res)
+    const pictures = res.map(({ url }) => {
+      url = `${process.env.API}${url}`
+      return { url }
+    })
+    commit('setPictures', pictures)
   })
 }
